ed7af560ae3bbd286ffcf1015a986652a0938fec
OMAR <s11925044@stu.najah.edu>
2024-09-04 19:45:48 +0300

    Create My Bids endpoint and view.

--- AuctionApp/Data/Services/BidsService.cs
+++ AuctionApp/Data/Services/BidsService.cs
@@ -1,4 +1,5 @@
 ﻿using AuctionApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionApp.Data.Services
 {
@@ -15,6 +16,13 @@ namespace AuctionApp.Data.Services
         {
             _context.Bids.Add(bid);
             await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Bid> GetAll()
+        {
+            var applicationDbContex = from a in _context.Bids.Include(l => l.Listing).ThenInclude(l => l.User)
+                                      select a;
+            return applicationDbContex;
         }
     }
 }
--- AuctionApp/Data/Services/IBidsService.cs
+++ AuctionApp/Data/Services/IBidsService.cs
@@ -5,5 +5,6 @@ namespace AuctionApp.Data.Services
     public interface IBidsService
     {
         Task Add(Bid bid);
+        IQueryable<Bid> GetAll();
     }
 }
